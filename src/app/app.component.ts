69505e4a7b11b0b2b8d61187d3658af319c16955
dennned <makarenkodeniz@gmail.com>
2020-05-03 20:57:02 +0200

    transition expression

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,20 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         background:'red',
         transform: 'scale(1.3)'
       })),
+      state('special', style({
+        background:'orange',
+        transform: 'scale(0.5)',
+        borderRadius: '50%'
+      })),
       transition('start => end', animate(450)),
       transition('end => start', animate('800ms ease-in-out')),
+      transition('special <=> *', [
+        style({background:'green'}),
+        animate('1s', style({
+          background:'pink'
+        })),
+        animate(750)
+      ]),
     ])
   ]
 })
